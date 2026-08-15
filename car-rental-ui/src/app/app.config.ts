5fec047e84cc24b735c7dcf02e1f9a17b446ab61
shalyanharutyun <shalyanharutyun9@gmail.com>
2026-08-15 10:27:01 +0000

    Fix i18n loading and auth oauth routing

--- car-rental-ui/src/app/app.config.ts
+++ car-rental-ui/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
-import { provideTranslateService } from '@ngx-translate/core';
+import { TranslateService, provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { firstValueFrom } from 'rxjs';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth.interceptor';
 import { DEFAULT_LANG, SUPPORTED_LANGS, detectInitialLang } from './core/i18n';
@@ -19,6 +20,15 @@ export const appConfig: ApplicationConfig = {
       loader: provideTranslateHttpLoader({ prefix: '/i18n/', suffix: '.json' }),
       fallbackLang: DEFAULT_LANG,
       lang: detectInitialLang()
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      const lang = detectInitialLang();
+
+      translate.addLangs([...SUPPORTED_LANGS]);
+
+      return firstValueFrom(translate.setFallbackLang(DEFAULT_LANG))
+        .then(() => firstValueFrom(translate.use(lang)));
     })
   ]
 };
